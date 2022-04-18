9de9608bd9620e7f4c21a01067f9d79694890061
Spencer Kline <spencerk98@gmail.com>
2022-04-18 08:36:18 -0400

    type hints

--- gh-mac.py
+++ gh-mac.py
@@ -4,6 +4,7 @@ import base64
 import time
 import random, string
 import datetime
+import json
 
 NEW_LINE = "\n"
 
@@ -13,7 +14,7 @@ secret = "qwfXhRvs6r5xJEEK37KO+qvSGvAijtJ/vG8xim6e+xo="
 issue_date = 1443126493378
 
 nonce = "7349622:vCZfJEjW"
-body = '{"task_id":"ae463g"}'
+body = json.dumps({"task_id":"ae463g"})
 request_method = "GET"
 host = "pos-api-url.grubhub.com"
 port = 443
@@ -21,14 +22,14 @@ uri = "/pos/v1/merchant/11446280/orders"
 ext = ""
 
 # Generate Nonce Method
-def _generate_nonce():
+def _generate_nonce() -> str:
     t = time.time()
     token = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
     new_nonce = f"{round(issue_date - t)}:{token}"
     return new_nonce
   
 # Hash body Method
-def _hash_body(body):
+def _hash_body(body: str) -> str:
     if not body:
         return ""
     return base64.b64encode(hashlib.sha256(bytes(body, encoding="utf-8")).digest())
@@ -44,14 +45,14 @@ def _normalize_request(nonce: str, request_method: str, request_uri: str, host: 
           f"{ext}{NEW_LINE}")
 
 # Hash normalized request Method
-def _hash_normalized_request(request, secret):
+def _hash_normalized_request(request, secret) -> str:
     requestBytes = bytes(request, encoding='utf-8')
     secretBytes = bytes(secret, encoding='utf-8')
     signature = base64.b64encode(hmac.new(secretBytes, requestBytes, digestmod=hashlib.sha256).digest())
     return signature
 
 # Create header value Method
-def _create_header_value(client_id, nonce, body_hash, ext, mac):
+def _create_header_value(client_id: str, nonce: str, body_hash: str, ext: str, mac: str) -> str:
     header_value = ""
     header_value += f"MAC id=\"{client_id}\",nonce=\"{nonce}"
 
@@ -64,7 +65,7 @@ def _create_header_value(client_id, nonce, body_hash, ext, mac):
     header_value += f"\",mac=\"{mac}\""
     return header_value
 
-def create_authentication_header(body, method, uri):
+def create_authentication_header(body: str, method: str, uri: str) -> str:
   auth_nonce = _generate_nonce()
   body_hash = _hash_body(body).decode('utf-8')
   normal_request = _normalize_request(nonce, method, 
